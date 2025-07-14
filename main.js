19009c738dc1338e421dbcac2e6d2cee160e83c8
Reena kumari <reenakvs1@gmail.com>
2025-07-14 13:14:30 +0530

    nabar

---
+++ main.js
@@ -0,0 +1,32 @@
+// Grab elements
+const selectElement = selector =>{
+    const element= document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`Something went wrong, make sure ${selector} exist! or is typed correctly`)
+}
+
+// Nav styles on scroll
+const scrollHeader = () =>{
+    const headerElement = selectElement("#header");
+    if(this.scrollY >= 15){
+        headerElement.classList.add("activated");
+    }else{
+        headerElement.classList.remove("activated");
+    }
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+// Open menu and search pop up 
+const menuToggleIcon = selectElement("#menu-toggle-icon");
+
+const toggleMenu = () => {
+  const mobileMenu = selectElement("#menu");
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener("click", toggleMenu);
+
+
+
